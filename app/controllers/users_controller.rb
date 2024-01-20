bf116d617f97b9c4b2490f47130dddf5ecc41b27
EC2 Default User <ec2-user@ip-172-31-5-92.ap-northeast-1.compute.internal>
2024-01-20 06:26:17 +0000

    [finish]4-15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
   def show
-    @users = PostImage.find(params[:id])
+    @user = User.find(params[:id])
     @post_images = @user.post_images
   end
 
   def edit
+    @user = PostImage.find(params[:id])
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,7 +5,7 @@ class PostImage < ApplicationRecord
 
   def get_image
     unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_imag.jpg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
