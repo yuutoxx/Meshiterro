e7006a74f8b0d7c9fd0dd73b964495c8df6f3f7f
EC2 Default User <ec2-user@ip-172-31-5-92.ap-northeast-1.compute.internal>
2024-01-21 18:15:09 +0000

    [finish]4-24

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Kaminari.configure do |config|
-  # config.default_per_page = 5
+  # config.default_per_page = 25
   # config.max_per_page = nil
   # config.window = 4
   # config.outer_window = 0
@@ -11,4 +11,7 @@ Kaminari.configure do |config|
   # config.param_name = :page
   # config.max_pages = nil
   # config.params_on_first_page = false
+
+    config.default_per_page = 5
+
 end
