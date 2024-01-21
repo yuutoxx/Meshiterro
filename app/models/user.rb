5b28b3f1e00f789de917836629f4724bc0b3a9a3
EC2 Default User <ec2-user@ip-172-31-5-92.ap-northeast-1.compute.internal>
2024-01-21 13:57:10 +0000

    [finish]4-20

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
-
+  
+  
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
